51bb727bf648db5b86843d98aa75542159bd3540
Maxence Langlois <122369999+MaxenceLanglois@users.noreply.github.com>
2023-01-23 15:24:23 -0500

    Update index.js
    added variable output

--- js/index.js
+++ js/index.js
@@ -1,27 +1,42 @@
+//initial setup
 let a = 3;
 let b = 5;
 let c;
 
-let output = 'let a = 3;\n';
-output += 'let b = 5;\n';
-output += 'let c;\n';
-output += '----------\n';
-output += ('a + b = ' + (a + b) + '\n');
-output += ('a - b = ' + (a - b) + '\n');
-output += ('a * b = ' + (a * b) + '\n');
-output += ('a / b = ' + (a / b) + '\n');
-output += ('a % b = ' + (a % b) + '\n');
-output += ('(a += b): ' + (a += b) + '\n');
-output += ('(a -= b): ' + (a -= b) + '\n');
-output += ('(a *= b): ' + (a *= b) + '\n');
-output += ('(a /= b): ' + (a /= b) + '\n');
-output += ('(a %= b): ' + (a %= b) + '\n');
-output += ('(a == b): ' + (a == b) + '\n');
-output += ('(a != b): ' + (a != b) + '\n');
-output += ('(a > b): ' + (a > b) + '\n');
-output += ('(a < b): ' + (a < b) + '\n');
-output += ('(!a && !c): ' + (!a && !c) + '\n');
-output += ('(!a || !c): ' + (!a || !c) + '\n');
+let printing = 'let a = 3;\n';
+printing += 'let b = 5;\n';
+printing += 'let c;\n';
+printing += '----------\n';
+printing += ('a + b = ' + (a + b) + '\n');
+printing += ('a - b = ' + (a - b) + '\n');
+printing += ('a * b = ' + (a * b) + '\n');
+printing += ('a / b = ' + (a / b) + '\n');
+printing += ('a % b = ' + (a % b) + '\n');
+printing += ('(a += b): ' + (a += b) + '\n');
+printing += ('(a -= b): ' + (a -= b) + '\n');
+printing += ('(a *= b): ' + (a *= b) + '\n');
+printing += ('(a /= b): ' + (a /= b) + '\n');
+printing += ('(a %= b): ' + (a %= b) + '\n');
+printing += ('(a == b): ' + (a == b) + '\n');
+printing += ('(a != b): ' + (a != b) + '\n');
+printing += ('(a > b): ' + (a > b) + '\n');
+printing += ('(a < b): ' + (a < b) + '\n');
+printing += ('(!a && !c): ' + (!a && !c) + '\n');
+printing += ('(!a || !c): ' + (!a || !c) + '\n');
+
+alert(printing);
+
+
+// // Declare variable first_name and assign it with value "Maxence".
+let first_name="Maxence";
+// // Declare variable last_name and assign it with value "Langlois".
+let last_name="Langlois";
+// // Declare variable email and assign it with my college email.
+let email = "lang0553@algonquinlive.com";
+// // Declare variable output.
+let output;
+
+output = ' My name is ' + first_name + ' ' + last_name + '.' + ' You can contact me at ' + ' ' + email + '.';
+
 
 alert(output);
-
